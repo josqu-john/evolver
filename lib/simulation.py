2c66d2d9953f24bdfab807575c6883cde34c245a
John <jgonline@live.de>
2017-05-26 21:37:02 +0200

    add classes in simulation.py

---
+++ lib/simulation.py
@@ -0,0 +1,15 @@
+class Simulation(object):
+    def __init__(self):
+        print "Foo"
+
+class Species(object):
+    def __init__(self):
+        print "Foo"
+
+class World(object):
+    def __init__(self):
+        print "Foo"
+
+class F_tree(object): # Family Tree for a Species
+    def __init__(self):
+        print "Foo"
