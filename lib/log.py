6b3008ca09ed5da90c2fa8b6a32a73bef07b8128
John <jgonline@live.de>
2017-05-29 20:20:17 +0200

    addet log.py

---
+++ lib/log.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import time
+
+log_txt = "Unkown"
+def make_log(f = log_txt):
+    date = give_date()
+    file_name = '%s' % (date)
+    file_location = 'log/%s' % (file_name)
+    touch('log/%s' % (file_name))
+    set_file(open(file_location, 'w'))
+    write_('/<--- BEGIN LOG AT %s: --->' % (date))
+
+def set_file(var):
+    global log_txt
+    log_txt = var
+def give_file():
+    return log_txt
+def write_only(var):
+    global log_txt
+    log_txt.write('\n')
+    log_txt.write('%s: %s' % (give_time(), var))
+def show(var):
+    print var
+def write_(var):
+    write_only(var)
+    show(var)
+def close_log():
+    write_('>--- END LOG AT %s: ---</' % (give_date()))
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
+def give_date():
+    date = time.strftime("%c")
+    return date
+def give_time():
+    t = time.strftime("%X")
+    return t
